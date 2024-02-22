833d41202dfc03ab828b69717ed00bec0c08352c
WebHav-bhardwaj <bhardwajvaibhav2611@gmail.com>
2024-02-22 10:41:29 +0530

    This is the first commit

--- src/javaExceptionExample.java
+++ src/javaExceptionExample.java
@@ -1,2 +1,9 @@
 public class javaExceptionExample {
+    public static void main(String[] args) {
+        try{
+            int a = 5/0;
+        }catch(ArithmeticException e ){
+            System.out.println("Cannot divide a number bu 0");
+        }
+    }
 }
